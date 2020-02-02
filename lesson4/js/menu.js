fa0e03a7fd11088cc7c82242d39a0c74fe198655
marie-roe <lmarie.roe@gmail.com>
2020-02-01 23:56:45 -0700

    initial page setup lesson4

---
+++ lesson4/js/menu.js
@@ -0,0 +1,6 @@
+const menuTab = document.querySelector(".menuTab");
+menuButton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector("*.navigation").classList.toggle("respond");
+}
