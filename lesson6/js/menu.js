30c4f826d9c956dd5ab9c62f7b8e1f63fb29d2a3
marie-roe <lmarie.roe@gmail.com>
2020-02-16 05:47:49 -0700

    lesson6 sitebuild

---
+++ lesson6/js/menu.js
@@ -0,0 +1,6 @@
+const hambutton = document.querySelector(".menuTab");
+hambutton.addEventListener("click", toggleMenu, false);
+
+function toggleMenu() {
+    document.querySelector(".navlist").classList.toggle("responsive");
+}
