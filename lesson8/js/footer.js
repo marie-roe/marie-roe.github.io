cfe7a6fdf772261d085c499f0c47cae4f8e98ff4
marie-roe <lmarie.roe@gmail.com>
2020-03-07 18:55:01 -0700

    Home page/ storm page updates

---
+++ lesson8/js/footer.js
@@ -0,0 +1,7 @@
+//current year
+let d= new Date().getFullYear();
+document.getElementById("currentyear").textContent = d;
+
+//last modified date
+let LastModif = new Date(document.lastModified);
+    document.getElementById("lastmodified").textContent = "Last Updated: " + LastModif;
