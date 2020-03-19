8540c76553dd1abab37799e019bb9e6594e90570
marie-roe <lmarie.roe@gmail.com>
2020-03-18 19:34:30 -0600

    update lesson10 folder

---
+++ lesson10/js/weatherforecast.js
@@ -0,0 +1,41 @@
+let weatherKey = "44294ac7db20b57cd171a76c2466d031";
+let id="";
+
+switch(document.getElementById('').innerHTML) {
+  case "Preston":
+    id = "5604473";
+    break;
+  case "SodaSprings":
+    id = "5607916";
+    break;
+  case "FishHaven":
+    id = "5585010";
+    break;
+}
+
+
+const apiURL = `https://api.openweathermap.org/data/2.5/forecast?id=${id}&units=imperial&APPID=${weatherKey}`;
+
+
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    let daysofWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    let fiveday = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+
+    for (let i=0; i<fiveday.length; i++) {
+      let d = new Date(fiveday[i].dt_txt);
+      let day  = daysofWeek[d.getDay()];
+
+      document.getElementById("day" + i).textContent = day;
+
+      const imagesrc =(`https://openweathermap.org/img/w/${fiveday[i].weather[0].icon}.png`);
+
+      document.getElementById("icon" + i).setAttribute("src", "imagesrc");
+      document.getElementById("icon" + i).setAttribute("alt", fiveday[i].weather[0].description);
+      
+    }
+  });
+
+
+
