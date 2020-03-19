5739d257691d98a1d0f8545f4ef6a3677fcba11d
marie-roe <lmarie.roe@gmail.com>
2020-03-18 19:58:55 -0600

    update

--- lesson10/js/currentweather.js
+++ lesson10/js/currentweather.js
@@ -1,11 +1,11 @@
 let weatherKey = "44294ac7db20b57cd171a76c2466d031";
 let id="";
 
-switch(document.getElementById('').innerHTML) {
-  case "Preston":
+switch(document.getElementById('town').innerHTML) {
+  case "Preston, Idaho":
     id = "5604473";
     break;
-  case "SodaSprings":
+  case "Soda Springs, Idaho":
     id = "5607916";
     break;
   case "FishHaven":
