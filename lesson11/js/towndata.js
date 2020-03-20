9e2b1d06d5e7c1cd4589de58e3a08cf268c2eddf
marie-roe <lmarie.roe@gmail.com>
2020-03-19 22:32:12 -0600

    update lesson11

--- lesson11/js/towndata.js
+++ lesson11/js/towndata.js
@@ -8,24 +8,24 @@ fetch(requestURL)
    
     const towns = jsonObject['towns'];
 
-     for (let i=0; i<2; i++) {
+    for (let i=0; i<towns.length; i++) {
+     if (towns[i].name == "Preston" || towns[i].name == "Soda Springs" || towns[i].name == "Fish Haven") {
       let card = document.createElement('section');
       let h2 = document.createElement('h2');
       let h3 = document.createElement('h3');
-      let year = document.createElement('p');
+      let year = document.createElement('p'); 
       let pop = document.createElement('p');
       let rain  = document.createElement('p');
-      //let image = document.createElement('img');//
+      let image = document.createElement('img');
       
 
       h2.textContent = towns[i].name; 
       h3.textContent = towns[i].motto;
-    
-      //image.setAttribute('src', images/towns[i].photo);//
-      //image.setAttribute('alt', );//
       year.textContent = "Year Founded" + ": " + towns[i].yearFounded;
       pop.textContent = "Population" + ": " + towns[i].currentPopulation;
       rain.textContent = "Annual Rainfall" + ": " + towns[i].averageRainfall + " inch(es)";
+      image.setAttribute('src', 'images/' + towns[i].photo);
+      image.setAttribute('alt',towns[i].name);
 
   
       card.appendChild(h2);
@@ -33,10 +33,10 @@ fetch(requestURL)
       card.appendChild(year);
       card.appendChild(pop);
       card.appendChild(rain);
-      //card.appendChild(image);//
+      card.appendChild(image);
       
 
       document.querySelector('.towns').appendChild(card);
-  
+     }
   }
 });
